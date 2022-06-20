78cc3a3b55295aa814000252da2fe64dab75b177
boiscljo <jonathan.boisclair@uqtr.ca>
2022-06-20 17:21:55 +0000

    Deploying to docs from @ ObsidianMakerDevelopment/ObsidianMaterial@ca1c4d16dd5cb0790619cc5ca06e2912ac9939ef 🚀

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.moyskleytech.obsidian.material"},{"l":"com.moyskleytech.obsidian.material.implementations"},{"l":"com.moyskleytech.obsidian.material.parsers"}];updateSearchResults();
